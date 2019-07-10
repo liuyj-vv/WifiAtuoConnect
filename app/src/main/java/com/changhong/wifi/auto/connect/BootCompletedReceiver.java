28ed36741067e76759503c600e5e824ea2efc3ec
liuyj <353381808@qq.com>
2019-07-10 15:44:51 +0800

    Toast显示

--- app/src/main/java/com/changhong/wifi/auto/connect/BootCompletedReceiver.java
+++ app/src/main/java/com/changhong/wifi/auto/connect/BootCompletedReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     String TAG = BootCompletedReceiver.class.getSimpleName();
@@ -12,6 +13,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         Log.e(TAG, Thread.currentThread().getStackTrace()[2].getMethodName()+"["+Thread.currentThread().getStackTrace()[2].getLineNumber()+"]");
 
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Toast.makeText(context, "收到开机广播", Toast.LENGTH_LONG).show();
             Intent i = new Intent(context, WifiAutoConnectService.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startService(i);
