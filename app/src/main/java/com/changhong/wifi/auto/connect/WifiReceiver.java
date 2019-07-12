f6d77dbe7a90841c6f60266fdda8974205066cb7
liuyj <353381808@qq.com>
2019-07-12 17:58:19 +0800

    WifiHelper 创建，配置wifi密码的类

--- app/src/main/java/com/changhong/wifi/auto/connect/WifiReceiver.java
+++ app/src/main/java/com/changhong/wifi/auto/connect/WifiReceiver.java
@@ -59,7 +59,7 @@ class WifiReceiver extends BroadcastReceiver {
 
             Log.i(TAG, "测试广播接收: " +action);
 
-            int netId = wifiManager.addNetwork(createWifiConfig("test991", "123456789", 2));
+            int netId = wifiManager.addNetwork(WifiHelper.createWifiConfig(wifiManager, "test991", "123456789", 2));
             Log.d(TAG, "netId: " + netId);
             boolean enable = wifiManager.enableNetwork(netId, true);
             Log.d(TAG, "enable: " + enable);
@@ -74,64 +74,4 @@ class WifiReceiver extends BroadcastReceiver {
         new WifiConfiguration();
     }
 
-    private static final int WIFICIPHER_NOPASS = 0;
-    private static final int WIFICIPHER_WEP = 1;
-    private static final int WIFICIPHER_WPA = 2;
-
-    private WifiConfiguration createWifiConfig(String ssid, String password, int type) {
-        //初始化WifiConfiguration
-        WifiConfiguration config = new WifiConfiguration();
-        config.allowedAuthAlgorithms.clear();
-        config.allowedGroupCiphers.clear();
-        config.allowedKeyManagement.clear();
-        config.allowedPairwiseCiphers.clear();
-        config.allowedProtocols.clear();
-
-        //指定对应的SSID
-        config.SSID = "\"" + ssid + "\"";
-
-        //如果之前有类似的配置
-        WifiConfiguration tempConfig = isExist(ssid);
-        if(tempConfig != null) {
-            //则清除旧有配置
-            wifiManager.removeNetwork(tempConfig.networkId);
-        }
-
-        //不需要密码的场景
-        if(type == WIFICIPHER_NOPASS) {
-            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-            //以WEP加密的场景
-        } else if(type == WIFICIPHER_WEP) {
-            config.hiddenSSID = true;
-            config.wepKeys[0]= "\""+password+"\"";
-            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
-            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-            config.wepTxKeyIndex = 0;
-            //以WPA加密的场景，自己测试时，发现热点以WPA2建立时，同样可以用这种配置连接
-        } else if(type == WIFICIPHER_WPA) {
-            config.preSharedKey = "\""+password+"\"";
-            config.hiddenSSID = true;
-            config.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-            config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-            config.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-            config.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-            config.status = WifiConfiguration.Status.ENABLED;
-        }
-
-        return config;
-    }
-
-    private WifiConfiguration isExist(String ssid) {
-        List<WifiConfiguration> configs = wifiManager.getConfiguredNetworks();
-
-        for (WifiConfiguration config : configs) {
-            if (config.SSID.equals("\""+ssid+"\"")) {
-                return config;
-            }
-        }
-        return null;
-    }
 }
